0124506bf2a178b410f42db932f17ea3df1313eb
spiderhuntstech <testing.second.dev@gmail.com>
2024-07-10 14:19:01 +0500

    Update config.php

--- layouts/config.php
+++ layouts/config.php
@@ -1,9 +1,9 @@
 <?php
 
-define('DB_SERVER', 'db-mysql-nyc3-00281-do-user-16900411-0.a.db.ondigitalocean.com');
-define('DB_USERNAME', 'doadmin');
-define('DB_PASSWORD', 'AVNS_9dgqKkRWMnOWR4xcEAI');
-define('DB_NAME', 'defaultdb');
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'inst_creditcard');
+define('DB_PASSWORD', 'inst_creditcard');
+define('DB_NAME', 'inst_creditcard');
 
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
